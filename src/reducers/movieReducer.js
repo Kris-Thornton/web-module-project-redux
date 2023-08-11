6b4e8dda1f9f4b1c7813a514d77f912feec8f38d
Kris Thornton <Kristhornton@ymail.com>
2023-08-11 05:39:44 -0700

    Finished Project

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -10,10 +10,20 @@ const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
+                ...state,
                 movies: state.movies.filter(item=>(action.payload !== item.id))
             }
         default:
             return state;
+        case ADD_MOVIE:
+            const newMovie = {
+                ...action.payload,
+                id:Date.now
+            }
+            return {
+                ...state,
+              movies: [...state.movies, newMovie]  
+            }
     }
 }
 
